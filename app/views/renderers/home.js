199ad3d9947b1e5f4457d9c7cdf8ec2ca841f536
unknown <4de4dr1>
2023-05-21 18:20:44 +0200

    Home view

---
+++ app/views/renderers/home.js
@@ -0,0 +1,53 @@
+
+// Listener for when the DOM is loaded.
+$(() => {
+    // Listener for when you click on one of the two tabs (TMB or Diet).
+    $('div.box-menu-items span').on('click', function (e) {
+        e.preventDefault();
+        // Picks up the name of the clicked tab.
+        const tabName = $(this).text().toLowerCase();
+
+        // Modifies the style of the tabs depending on which one is clicked.
+        // First remove the tab-selected class to the two tabs.
+        $('div.box-menu-items span').removeClass('tab-selected'); 
+        // Then apply the tab-selected class to the selected tab.
+        $(`div.box-menu-items span[class*="tab-${tabName}"]`).addClass('tab-selected');
+
+        // Call to the method that loads the page tmb.html or midieta.html in the view.
+        loadSection(tabName);
+    });
+
+    // The TMB section is displayed by default.
+    loadSection('tmb');
+
+    // Listener for when the Food button is clicked.
+    $('#btn-food').on('click', function () {
+        window.api.send('open-food');
+    });
+});
+
+// It listens to the userData call to receive the logged-in user's data and paints it in the view.
+window.api.receive('userData', (res) => {
+    $('.username span').html(`<i class='bi bi-arrow-down-short'></i> ${res.USERNAME}`);
+});
+
+/**
+ * Method that is in charge of loading the page tmb.html or diet.html in the content of the view 
+ * depending on the section that has been clicked.
+ */
+function loadSection(tabName) {
+    $.get(`./${tabName}.html`, (data) => {
+        $('.load-content').html(data);
+    });
+}
+
+/**
+ * Listener to enable/disable the Food button.
+ */
+window.api.receive('stateFoodButton', (state) => {
+    if(state) {
+        $('#btn-food').prop('disabled', true);
+    } else {
+        $('#btn-food').prop('disabled', false);
+    }
+});
